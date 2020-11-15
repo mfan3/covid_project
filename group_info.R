e9dda6f710c2dc75b5c5cba76795f444f55324dd
mfan <mufan@ucsd.edu>
2020-11-14 16:05:43 -0800

    updated group info

--- group_info.R
+++ group_info.R
@@ -1,6 +1,6 @@
 names <- c("Hyojeong (Jenny) Yoo", "Juanshu Wu",  "Muqing Fan", "Xiangyu (Alaric) Wang", "Yuchen Liu")
 role <- c("psych grad 1st", "psych grad 1st", "psych grad 1st", "undergrad", "undergrad")
-contact <- c("h4yoo", "juw1365", "mufan", "xiw404", "yul862")
+contact <- c("h4yoo", "juw135", "mufan", "xiw404", "yul862")
 covid_group <- data.frame(names, role, contact)
 
 # write.csv(covid_group,"C:\\Users\\fan\\Desktop\\covid_group.csv")
